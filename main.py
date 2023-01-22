0ad19d0b107595348b1325df3a2d5f7a9d163608
fernunex <fer.bsof@outlook.com>
2023-01-21 22:10:01 -0600

    path_operations_structure_users

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Python
 from uuid import UUID
 from datetime import date
-from typing import Optional
+from typing import Optional, List
 
 # Pydantic
 from pydantic import BaseModel
@@ -9,15 +9,82 @@ from pydantic import EmailStr
 from pydantic import Field
 
 # FastAPI
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 # Models
-from models import * #specify
+from models import UserBase, UserLogin, User, Tweet
+
+# Path operations 
 
 app = FastAPI()
-
-
 
 @app.get(path="/")
 def home():
     return {"Twitter API": "Working!"}
+
+## AUTHENTICATION
+
+@app.post(
+    path="/auth/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=['Authentication']
+)
+def auth_signup():
+    pass
+
+@app.post(
+    path="/auth/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags=['Authentication']
+)
+def auth_login():
+    pass
+
+## USERS
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all users",
+    tags=['Users']
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a User",
+    tags=['Users']
+)
+def show_a_user():
+    pass
+
+@app.delete(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a User",
+    tags=['Users']
+)
+def delete_a_user():
+    pass
+
+@app.put(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a User",
+    tags=['Users']
+)
+def update_a_user():
+    pass
+
+
+
+## Tweets
