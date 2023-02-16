b932d3f2a9869c26f6c3d36733401cea5c624004
fernunex <fer.bsof@outlook.com>
2023-02-15 20:25:18 -0600

    users_signup

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,8 +1,11 @@
+# Python
+import json
+
 # FastAPI
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Body
 
 # Models
-from models import User
+from models import User, UserRegister
 
 router = APIRouter(
     prefix='/auth',
@@ -18,7 +21,7 @@ router = APIRouter(
     status_code=status.HTTP_201_CREATED,
     summary="Register a User"
 )
-def auth_signup():
+def auth_signup(user: UserRegister = Body(...)):
     """
     Signup
 
@@ -28,13 +31,24 @@ def auth_signup():
         - Request body parameter
             -user : UserRegister
     
-    Return a json with the badic user information:
+    Return a json with the basic user information:
         - user_id: UUID
         - email: EmailStr
         - first_name: str
         - last_name: str
         - birth_date: date
     """
+    with open("users.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        user_dict = user.dict()
+        user_dict["user_id"] = str(user_dict["user_id"])
+        user_dict["birth_date"] = str(user_dict["birth_date"])
+        results.append(user_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return user
+
+
 
 ### Login a user
 @router.post(
--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
-from .User import UserLogin, User
+from .User import UserLogin, User, UserRegister
 from .Tweet import Tweet
