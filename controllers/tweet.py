2650f440071fa06f8f96a27cadab6973c2165f97
fernunex <fer.bsof@outlook.com>
2023-02-19 21:58:51 -0600

    tweet_post

--- controllers/tweet.py
+++ controllers/tweet.py
@@ -1,8 +1,9 @@
 # Python
 from typing import List
+import json
 
 # FastAPI
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Body
 
 # Models
 from models import Tweet
@@ -33,8 +34,41 @@ def get_all_tweets():
     status_code=status.HTTP_201_CREATED,
     summary="Post a Tweet"
     )
-def post_a_tweet():
-    pass
+def post_a_tweet(tweet: Tweet = Body(...)):
+    """
+    Post a Tweet
+
+    This path operation post a tweet in the app
+
+    Parameters:
+        - Request body parameter
+            - tweet : Tweet 
+    
+    Return a json with the basic tweet information:
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+
+        if tweet_dict["updated_at"]:
+            tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        
+        tweet_dict['by']['user_id'] = str(tweet_dict['by']['user_id'])
+        tweet_dict['by']['birth_date'] = str(tweet_dict['by']['birth_date'])
+
+
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @router.get(
