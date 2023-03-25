978284b61635ebc51028d24de69bd420e1046151
fernunex <fer.bsof@outlook.com>
2023-03-25 16:36:11 -0600

    Home finished

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Python
+import json
+
 # FastAPI
 from fastapi import FastAPI
 
@@ -14,4 +17,19 @@ app.include_router(router)
     path='/'
 )
 def home():
-    return {'Hi':'I am working'}
+    """
+    This path operation shows all tweets in the app
+
+    Parameters:
+        -
+    
+    Returns a json list with all tweets in the app, with the following keys:
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read()) # list of dicts
+        return results # Fast api can convert this to a json
